facb2361dfb756e0786551ee990d0eaa667932c2
muhammad ziada <engziada@gmail.com>
2024-05-08 00:13:16 +0300

    chore: Update scripts.html and custom-style.css, and custom_script.js

--- apps/static/assets/js/custom_script.js
+++ apps/static/assets/js/custom_script.js
@@ -18,3 +18,43 @@ document.getElementById('confirm-delete-btn').addEventListener('click', function
     };
     xhr.send();
 });
+
+// function toggleMenu() {
+//     var navbarCollapse = document.querySelector('.navbar-collapse');
+//     navbarCollapse.style.display = navbarCollapse.style.display === 'block' ? 'none' : 'block';
+// }
+
+// function toggleDropdown(dropdownId) {
+//     var dropdownMenu = document.getElementById(dropdownId);
+//     dropdownMenu.style.display = dropdownMenu.style.display === 'block' ? 'none' : 'block';
+// }
+
+
+window.addEventListener('resize', function() {
+  if (window.innerWidth < 768) { // Adjust breakpoint as needed
+    // Expand all menus
+    var navItems = document.querySelectorAll('.navbar-collapse .dropdown');
+    navItems.forEach(item => {
+      item.classList.add('show'); // Add Bootstrap's 'show' class
+      item.querySelector('.dropdown-menu').classList.add('show');
+    });
+
+    // Disable collapse functionality
+    var collapseToggles = document.querySelectorAll('[data-bs-toggle="collapse"]');
+    collapseToggles.forEach(toggle => {
+        toggle.setAttribute('disabled', true);
+    });
+  } else {
+    // Restore default behavior (if desired)
+    var navItems = document.querySelectorAll('.navbar-collapse .dropdown');
+    navItems.forEach(item => {
+      item.classList.remove('show'); 
+      item.querySelector('.dropdown-menu').classList.remove('show');
+    });
+
+    var collapseToggles = document.querySelectorAll('[data-bs-toggle="collapse"]');
+    collapseToggles.forEach(toggle => {
+        toggle.removeAttribute('disabled');
+    });
+  }
+});
